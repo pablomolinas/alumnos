c90d1a7629f5d9be325d9c93f3d9a8a6e5aecdeb
Juan Pablo Molinas <juan.molinas@raona.com>
2022-06-14 13:30:48 -0300

    backend, migration SqlScript

---
+++ backend/Alumnos/Migrations/20220614123715_RunSqlScriptAddStudents.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Reflection;
+
+#nullable disable
+
+namespace Alumnos.Migrations
+{
+    public partial class RunSqlScriptAddStudents : Migration
+    {
+        private const string MIGRATION_SQL_SCRIPT_FILE_NAME = @"Migrations\20220614123715_RunSqlScriptAddStudents.sql";
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFile = Path.Combine(MIGRATION_SQL_SCRIPT_FILE_NAME);
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM dbo.StudentsSubjects");
+            migrationBuilder.Sql("DELETE FROM dbo.Subjects");
+            migrationBuilder.Sql("DELETE FROM dbo.Students");
+            migrationBuilder.Sql("DBCC CHECKIDENT(Subjects, RESEED, 0)");
+            migrationBuilder.Sql("DBCC CHECKIDENT(Students, RESEED, 0)");
+        }
+    }
+}
---
+++ backend/Alumnos/Migrations/20220614123715_RunSqlScriptAddStudents.sql
@@ -0,0 +1,25 @@
+﻿INSERT INTO dbo.Subjects (Name) 
+VALUES 
+('Analisis Matematico'),
+('Algebra'), 
+('Fisica'), 
+('Estadistica'),
+('Organizacion empresarial'), 
+('Laboratorio I');
+
+
+INSERT INTO dbo.Students (Name, Age)
+VALUES
+('Julian', 34),
+('Ignacio', 22),
+('Maria', 18),
+('Soledad', 42),
+('Fabio', 24);
+
+INSERT INTO dbo.StudentsSubjects (StudentId, SubjectId)
+VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5);
